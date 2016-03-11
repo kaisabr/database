f93557c6bb07ad64c390e0497b28a935975feadc
kaisabr <kaisa.rokne@gmail.com>
2016-03-11 12:41:54 +0100

    Update MaalHenter.java
    Lagt til forhåpentligvis noenlunde fungerende kode.

--- src/innlevering2/MaalHenter.java
+++ src/innlevering2/MaalHenter.java
@@ -1,5 +1,32 @@
 package innlevering2;
 
-public class MaalHenter {
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Maalhenter {
+  Maalhenter() {	
+    System.out.println("For hvilke øvelse ønsker du å hente ut mål?");
+		String exercise = Main.sc.nextLine();
+		System.out.println("Skriv inn startdato for progresjonsmålingen, på formen åååå-mm-dd: ");
+		String periodStart = Main.sc.nextLine();
+		System.out.println("Skriv inn sluttdato for progresjonsmålingen, på formen åååå-mm-dd: ");
+		String periodEnd = Main.sc.nextLine();
+		
+		
+		Statement stmt = conn.createStatement();
+        String query = "SELECT * FROM TRENINGSOEVELSE WHERE NAVN = " + exercise;
+        ResultSet rs = stmt.executeQuery(query);
+        while (rs.next()) {
+        	   System.out.println(String.format("Navn på øvelse: %s", rs.getString("NAVN")));
+        }
+        
+        Statement stmt1 = conn.createStatement();
+        String query1 = "SELECT * FROM TRENINGSØVELSE WHERE DATO >=" + periodEnd + "AND DATO <=" + periodStart;
+        ResultSet rs1 = stmt.executeQuery(query);
+        while (rs1.next()) {
+        	   System.out.println(String.format("Navn på øvelse: %s", rs.getString("NAVN")));
+        }
+        
+	}
 }
