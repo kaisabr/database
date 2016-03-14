df5c1bcfc4f384b0375be0f413fb97fcaadaf62f
Magnus <magnublo@stud.ntnu.no>
2016-03-14 11:18:35 +0100

    Har skrevet mer på MaalHenter

--- src/innlevering2/MaalHenter.java
+++ src/innlevering2/MaalHenter.java
@@ -15,20 +15,47 @@ public class MaalHenter extends DBConn{
 		System.out.println("Skriv inn sluttdato for progresjonsmålingen, på formen åååå-mm-dd: ");
 		String periodEnd = Main.sc.nextLine();
 		
-		
-		Statement stmt = conn.createStatement();
-        String query = "SELECT OEVELSESID FROM TRENINGSOEVELSE WHERE NAVN = " + exercise;
-        ResultSet rs = stmt.executeQuery(query);
-        while (rs.next()) {
-        	   System.out.println(String.format("Navn på øvelse: %s", rs.getString("NAVN")));
-        }
-        
-        Statement stmt1 = conn.createStatement();
-        String query1 = "SELECT * FROM MAAL AS M WHERE DATO >= " + periodEnd + " AND DATO <= " + periodStart + " AND M.OEVELSESID = TRENINGSOEVELSE.OEVELSESID WHERE ";
-        ResultSet rs1 = stmt1.executeQuery(query1);
-        while (rs1.next()) {
-        	   System.out.println(String.format("Navn på øvelse: %s", rs.getString("NAVN")));
-        }
-        
+		goalForStrength(exercise, periodStart, periodEnd);
+		goalforEndurance(exercise, periodStart, periodEnd);   
+      
+    }   
+
+private void goalforEndurance(String exercise, String periodStart, String periodEnd) throws SQLException {
+	Statement stmt = conn.createStatement();
+    String query = "SELECT O.NAVN, M.DATOFRA, M.DATOTIL, MF.AVSTAND, MF.VARIHETIMINUTTER, MF.HASTIGHET, MF.PULS"
+    		+ "FROM TRENINGSOEVELSE AS O, MAAL AS M, MAALFORUTHOLDENHET AS MF"
+    		+ "WHERE O.NAVN='" + exercise + "'AND M.OEVELSESID=O.OEVELSESID AND MF.OEVELSESID=M.OEVELSESID AND MF.DATOFRA=M.DATOFRA"
+    		+ "AND M.DATOFRA=>" + periodStart + "AND M.DATOTIL<=" + periodEnd + ";";
+    ResultSet rs = stmt.executeQuery(query);
+    while (rs.next()) {
+    	   System.out.println(String.format("Navn på øvelse: %s \n" 
+    			   +"Øvelsesnavn: %s \n"
+    			   +"Dato fra %s \n"
+    			   +"Dato til: %s \n"
+    			   +"Avstand: %s \n"
+    			   +"Varighet i minutter: %s \n"
+    			   +"Hastighet: %s \n"
+    			   +"Hastighet: %s \n"
+    			   +rs.getString("NAVN"), rs.getString("DATOFRA"),rs.getString("DATOTIL"),rs.getString("AVSTAND"),rs.getString("VARIHETIMINUTTER"),rs.getString("HASTIGHET"),rs.getString("PULS")));
+    }
+}
+
+private void goalForStrength(String exercise, String periodStart, String periodEnd) throws SQLException{
+	Statement stmt1 = conn.createStatement();
+    String query1 = "SELECT O.NAVN AS ØVELSESNAVN, M.DATOFRA, M.DATOTIL, MF.BELASTNING, MF.SETT, MF.REPETISJONER"
+    		+ "FROM TRENINGSOEVELSE AS O, MAAL AS M, MAALFORSTYRKE AS MF"
+    		+ "WHERE O.NAVN='" + exercise + "'AND M.OEVELSESID=O.OEVELSESID AND MF.OEVELSESID=M.OEVELSESID AND MF.DATOFRA=M.DATOFRA"
+    		+ "AND M.DATOFRA>='" + periodStart + "' AND M.DATOTIL<='" + periodEnd + "';";
+    ResultSet rs1 = stmt1.executeQuery(query1);
+    while (rs1.next()) {
+ 	   System.out.println(String.format("Navn på øvelse: %s \n" 
+ 			   +"Øvelsesnavn: %s \n"
+ 			   +"Dato fra %s \n"
+ 			   +"Dato til: %s \n"
+ 			   +"Belastning: %s \n"
+ 			   +"Sett: %s \n"
+ 			   +"Repetisjoner: %s \n"
+ 			   +rs1.getString("NAVN"), rs1.getString("DATOFRA"),rs1.getString("DATOTIL"),rs1.getString("BELASTNING"),rs1.getString("SETT"),rs1.getString("REPETISJONER")));
+		}
 	}
 }
